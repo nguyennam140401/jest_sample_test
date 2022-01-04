7b9d1a9b18b949a35ba0bc5fc02dea4440f1745f
nam140401 <nguyennam140401@gmail.com>
2022-01-04 22:49:06 +0700

    update

---
+++ __tests__/runTest.js
@@ -0,0 +1,21 @@
+const ExcelJS = require('exceljs')
+const workbook = new ExcelJS.Workbook()
+var table
+var arr = []
+beforeEach(() => {
+    workbook.xlsx.readFile('a.xlsx').then((data) => {
+        table = data.getWorksheet().getSheetValues()
+        for (let i = 2; i < table.length; i++) {
+            for (let j = 1; j < table[1].length; j++) {
+                var obj
+                obj = { ...obj, [table[1][j]]: table[i][j] }
+            }
+            arr.push(obj)
+        }
+        console.log(arr)
+    })
+})
+
+test('sum ', () => {
+    expect(4).toBe(4)
+})
